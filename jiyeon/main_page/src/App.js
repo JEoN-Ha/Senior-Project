d7393e3e0fe539b797d0663ff793bd97f2281377
kijiyeon <rlwldus305@naver.com>
2020-12-12 20:06:36 +0900

    nomadCoder #1,#2예제 실습

--- jiyeon/main_page/src/App.js
+++ jiyeon/main_page/src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from "prop-types";
+
+const foodILike = [
+  {
+    id :1,
+    name:"BigMac",
+    image:
+    "https://dimg.donga.com/wps/NEWS/IMAGE/2020/07/31/102251437.1.jpg",
+    rating:5
+  },
+  {
+    id:2,
+    name:"SangsChiCom",
+    image:
+    "https://pbs.twimg.com/media/DXcWN4lVwAA6nKm.jpg",
+    rating:4.9
+  },
+  {
+    id:3,
+    name:"MacDhicken",
+    image:
+    "https://img.insight.co.kr/static/2019/10/09/700/y90e2dht297mz9vrd93c.jpg",
+    rating:4.8
+  },
+  {
+    id:4,
+    name:"BaToDi",
+    image:
+    "https://pbs.twimg.com/media/EDxF8_cVAAA-HWZ.png",
+    rating:4.7
+  },
+];
+
+function Food({name, picture, rating}){
+  return <div>
+    <h2> I Like {name}</h2>
+    <h4>{rating}/5.0</h4>
+    <img src = {picture} alt = {name}/>
+  </div>
+}
+
+Food.propTypes = {
+  name : PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.string
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+  <div> 
+      {foodILike.map(dish => (
+        <Food 
+        key = {dish.id}
+        name = {dish.name} 
+        picture = {dish.image} 
+        rating ={dish.rating}/>
+      ))}
+      
+  </div>
   );
 }
 
--- jiyeon/main_page/src/index.js
+++ jiyeon/main_page/src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />,document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//render안에 들어있는 것들: component 
+//component는 React와 함께 동작 , HTML을 반환하는 함수
+//<APP/> <-- 이런형태만 React랑 동작할 수 있음 
+//JavaScript와 HTML사이의 이러한 조합을 jsx라고 부름
+
+
