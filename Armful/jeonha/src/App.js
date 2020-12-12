309d32e07f78604ea597a8cd29577c401378748f
dkfma4915 <52308823+dkfma4915@users.noreply.github.com>
2020-12-13 01:00:12 +0900

    Component 사용하여 기본적인 디자인
    제목, 리스트

--- Armful/jeonha/src/App.js
+++ Armful/jeonha/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+/* Component를 만드는 틀
+class Subject extends Component {
+  render() {
+    return ();
+  }
+}
+*/
+
+class Subject extends Component {   //Subject라는 태그를 생성
+  render() {
+    return (
+      <header>
+        <h1>{this.props.title}</h1>
+        <h3>{this.props.sub}</h3>
+        <h3>{this.props.desc}</h3>
       </header>
-    </div>
-  );
+    );
+  }
+}
+
+class List extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li><a href="1.html">메뉴판</a></li>
+          <li><a href="2.html">주문내역</a></li>
+          <li><a href="3.html">장바구니</a></li>
+          <li><a href="4.html">쿠폰</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class App extends Component {   
+  render() {
+    return (
+      <div className="App">
+      <Subject title="JEoN-Ha" sub="안녕하세요. JEoN-Ha입니다."
+      desc="Untact로 안전하게 이용이 가능한 무인 드라이브 스루입니다."></Subject>
+      <List></List>
+      </div>
+    );
+  }
 }
 
 export default App;
