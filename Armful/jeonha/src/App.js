6aec2309f9d3991c1341142a9c0c6617d09f686e
dkfma4915 <52308823+dkfma4915@users.noreply.github.com>
2020-12-13 17:32:26 +0900

    클릭 이벤트 추가

--- Armful/jeonha/src/App.js
+++ Armful/jeonha/src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
     super(props);
     this.state = {
       mode : 'welcome',  //현재 어떤 페이지에 있는지 구별하기위해
+      selected_content_id:2,
       subject : {title:'JEoN-Ha', sub:'안녕하세요. JEoN-Ha입니다.', 
         desc:'Untact로 안전하게 이용이 가능한 무인 드라이브 스루입니다.'},
       welcome : {title:'Welcome', desc:'Hello, customer!'},
@@ -35,16 +36,36 @@ class App extends Component {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
     } else if(this.state.mode === 'read'){
-      _title = this.state.Lists[0].title;
-      _desc = this.state.Lists[0].desc;
+      var i = 0;
+      while(i < this.state.Lists.length){
+        var data = this.state.Lists[i];
+        if(data.id === this.state.selected_content_id){
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
     }
     return (
       <div className="App">
       <Subject 
         title={this.state.subject.title} 
         sub={this.state.subject.sub}
-        desc={this.state.subject.desc}></Subject>
-      <List data={this.state.Lists}></List>
+        desc={this.state.subject.desc}
+        onChangePage={function() {
+          this.setState({mode:'welcome'});  //함수안에서 state 바꿀 때 무조건 setState 사용          
+        }.bind(this)}   //this를 함수안에서 쓸 때 무조건 쓰기
+        ></Subject>
+      <List 
+        onChangePage={function(id) {
+          this.setState({
+            mode:'read',
+            selected_content_id:Number(id)  //Number():문자를 숫자로 바꿔줌
+          });             
+        }.bind(this)}
+        data={this.state.Lists}
+        ></List>
       <Content title={_title} desc={_desc}></Content>
       </div>
     );
--- Armful/jeonha/src/components/Subject.js
+++ Armful/jeonha/src/components/Subject.js
@@ -4,7 +4,10 @@ class Subject extends Component {   //Subject라는 태그를 생성
     render() {
       return (
         <header>
-          <h1><a href="/">{this.props.title}</a></h1>
+          <h1><a href="/" onClick={function(e){
+            e.preventDefault();
+            this.props.onChangePage();  
+          }.bind(this)}>{this.props.title}</a></h1>
           <h3>{this.props.sub}</h3>
           <h3>{this.props.desc}</h3>
         </header>
