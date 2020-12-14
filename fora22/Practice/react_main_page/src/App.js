ff4726ca8da338efff16fc013ac88a330eadeb4e
팽대원 <fora22@kyonggi.ac.kr>
2020-12-14 17:01:08 +0900

    강의로 인한 Update

--- fora22/Practice/react_main_page/src/App.js
+++ fora22/Practice/react_main_page/src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import TOC from './components/TOC';
-import Content from './components/Content';
+import ReadContent from './components/ReadContent';
+import CreateContent from './components/CreateContent';
 import Subject from './components/Subject';
+import Control from './components/Control';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.max_content_id = 3;
     this.state = {
-      mode: 'read',
+      mode: 'create',
       selected_content_id:2,
       subject: { title: 'WEB', sub: 'World Wide Web!' },
       welcome: { title: 'Welcome', desc: 'Hello, React!!' },
-      contetns: [
+      contents: [
         { id: 1, title: 'HTML', desc: 'HTML is for information' },
         { id: 2, title: 'CSS', desc: 'CSS is for design' },
         { id: 3, title: 'JavaScript', desc: 'JavaScript is for interactive' }
@@ -21,14 +24,15 @@ class App extends Component {
     }
   }
   render() {
-    let _title, _desc = null;
+    let _title, _desc, _article = null;
     if (this.state.mode === 'welcome') {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
     } else if (this.state.mode === 'read') {
       let i = 0;
-      while (i < this.state.contetns.length) {
-        let data = this.state.contetns[i];
+      while (i < this.state.contents.length) {
+        let data = this.state.contents[i];
         if (data.id === this.state.selected_content_id) {
           _title = data.title;
           _desc = data.desc;
@@ -36,6 +40,21 @@ class App extends Component {
         }
         i = i + 1;
       }
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
+    } else if(this.state.mode ===  'create') {
+      _article = <CreateContent onSubmit={function (_title, _desc) {
+        // add content to this.state.contents
+        this.max_content_id = this.max_content_id + 1;
+        // this.state.contents.push({id:this.max_content_id, title:_title, desc:_desc}); // push는 원본에 직접 넣는 거지만
+        let _contents = this.state.contents.concat(   // concat은 원본은 건들지 않고 return 값에만 변화를 준다.
+          {id:this.max_content_id, title:_title, desc:_desc}
+        )
+        this.setState({
+          contents: _contents
+        });
+        console.log(_title, _desc);
+
+      }.bind(this)}></CreateContent>;
     }
     return (
       <div className="App">
@@ -52,8 +71,13 @@ class App extends Component {
             mode:'read',
             selected_content_id:Number(id)
           });
-        }.bind(this)} data={this.state.contetns}></TOC>
-        <Content title={_title} desc={_desc}></Content>
+        }.bind(this)} data={this.state.contents}></TOC>
+        <Control onChangeMode={function(_mode) {
+          this.setState({
+            mode:_mode
+          });
+        }.bind(this)} ></Control>
+        {_article}
       </div>
     );
   }
