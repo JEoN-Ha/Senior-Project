2fe7875a39e662418eb6f8a3aa38555223380cd2
dkfma4915 <52308823+dkfma4915@users.noreply.github.com>
2020-12-25 01:06:16 +0900

    react-redux 도입
    connect 사용

--- Armful/redux/node_modules/redux/src/containers/DisplayNumber.js
+++ Armful/redux/node_modules/redux/src/containers/DisplayNumber.js
@@ -1,17 +1,21 @@
 import DisplayNumber from "../Component/DisplayNumber";
-import React,{Component} from "react";
-import { compose } from "redux";
-import store from '../store';
+import {connect} from 'react-redux';
 
-export default class extends Component{
-    state = {number:store.getState().number}    // number 값을 store에서 가져와서 기본 세팅
-    constructor(props){
-        super(props);
-        store.subscribe(function(){     // store로부터 state가 변경됐다고 통보받음
-            this.setState({number:store.getState().number});
-        }.bind(this));
-    }
-    render(){
-        return <DisplayNumber number={this.state.number}></DisplayNumber>
-    }
-}
+export default connect()(DisplayNumber);    // 아래의 모든 코드를 하지 않고 컴포넌트를 Return함
+
+// import React,{Component} from "react";
+// import { compose } from "redux";
+// import store from '../store';
+
+// export default class extends Component{
+//     state = {number:store.getState().number}    // number 값을 store에서 가져와서 기본 세팅
+//     constructor(props){
+//         super(props);
+//         store.subscribe(function(){     // store로부터 state가 변경됐다고 통보받음
+//             this.setState({number:store.getState().number});
+//         }.bind(this));
+//     }
+//     render(){
+//         return <DisplayNumber number={this.state.number} unit={this.props.unit}></DisplayNumber>
+//     }
+// }
--- Armful/redux/node_modules/redux/src/index.js
+++ Armful/redux/node_modules/redux/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux';
+import store from './store';  // Provider를 이용해서 매번 Store를 import하는 귀찮은 일을 안해도 됨
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
