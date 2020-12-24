a80f8811ed72651c6adf94cc7fb412d60512adaa
dkfma4915 <52308823+dkfma4915@users.noreply.github.com>
2020-12-24 22:09:14 +0900

    redux 없는 컴포넌트 구조

--- Armful/redux/redux/src/App.js
+++ Armful/redux/redux/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import AddNumberRoot from "./Component/AddNumberRoot";
+import DisplayNumberRoot from "./Component/DisplayNumberRoot";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Root</h1>
+      <AddNumberRoot></AddNumberRoot>
+      <DisplayNumberRoot></DisplayNumberRoot>
     </div>
   );
 }
