4918e56a772e256be59b6b2a7008b7162fc52f02
dkfma4915 <52308823+dkfma4915@users.noreply.github.com>
2020-12-24 22:46:06 +0900

    컴포넌트 상태 연결

--- Armful/redux/redux/src/App.js
+++ Armful/redux/redux/src/App.js
@@ -3,14 +3,19 @@ import AddNumberRoot from "./Component/AddNumberRoot";
 import DisplayNumberRoot from "./Component/DisplayNumberRoot";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Root</h1>
-      <AddNumberRoot></AddNumberRoot>
-      <DisplayNumberRoot></DisplayNumberRoot>
-    </div>
-  );
+class App extends Component {
+  state = {number:0}
+  render(){
+    return (
+      <div className="App">
+        <h1>Root</h1>
+        <AddNumberRoot onClick={function(size){
+          this.setState({number:this.state.number + size});
+        }.bind(this)}></AddNumberRoot>
+        <DisplayNumberRoot number={this.state.number}></DisplayNumberRoot>
+      </div>
+    );
+  }
 }
 
 export default App;
