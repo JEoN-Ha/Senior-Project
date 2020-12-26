4abc632b64e8b4000e855560f820d7d00fcd0866
dkfma4915 <52308823+dkfma4915@users.noreply.github.com>
2020-12-26 23:29:32 +0900

    JEoN-Ha 제목 추가

--- Armful/jeonha_redux/src/App.js
+++ Armful/jeonha_redux/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'; 
 import './App.css';
+import Subject from './component/Subject';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* JEoN-Ha 제목 */}
+        <Subject></Subject>
+
+        {/* 로그인 폼 */}
+        {this.getLogin()}
+      </div>
+    )
+  }
 }
 
 export default App;
