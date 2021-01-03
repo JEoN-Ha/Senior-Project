cbe3d17f4cbb7e973fff8d2cb87d99be7889d0f8
dkfma4915 <52308823+dkfma4915@users.noreply.github.com>
2021-01-03 22:00:28 +0900

    결제수단, 주문정보 추가

--- Armful/jeonha_redux/src/component/BasketRoot.js
+++ Armful/jeonha_redux/src/component/BasketRoot.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Basket from '../containers/Basket_con';
+import Payment from '../containers/Payment_con';
 import store from '../store';
 
 export default class BasketRoot extends Component {
@@ -11,6 +12,14 @@ export default class BasketRoot extends Component {
         store.subscribe(function () {
             this.setState({mode_content:store.getState().mode_content});           
         }.bind(this));
+    }
+
+    getPayment(){
+        let _article = null;
+        if(this.state.mode_content === 'payment'){
+          _article = <Payment></Payment>
+        }
+        return _article;
     }
 
     getBasket(){
@@ -25,6 +34,7 @@ export default class BasketRoot extends Component {
         return (
             <div>
                 {this.getBasket()}
+                {this.getPayment()}
             </div>
         )
     }
---
+++ Armful/jeonha_redux/src/containers/Payment_con.js
@@ -0,0 +1,9 @@
+import Payment from "../component/Payment";
+import {connect} from 'react-redux';
+
+function mapDispatchToProps(dispatch){ 
+    return {
+        
+    }
+}
+export default connect(null,mapDispatchToProps)(Payment);
