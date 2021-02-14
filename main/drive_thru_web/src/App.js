c447ecdf20c324cb82e177ba17d8fb58ead4d465
fora22 <48875566+fora22@users.noreply.github.com>
2021-02-14 22:54:29 +0900

    fetch 추가

--- main/drive_thru_web/src/App.js
+++ main/drive_thru_web/src/App.js
@@ -10,6 +10,44 @@ import './component/Component.css';
 import SideBar from './component/SideBar';
 
 class App extends Component {
+  componentDidMount() {
+    const jeonhaUrl = 'http://localhost:4000';
+
+    const bodySignUP = {
+      userWebId: 'fora23',
+      userName: '팽대원',
+      pw: '1234',
+      phoneNum: '01011112222',
+      carId: '11가 1111'
+  };
+  
+  fetch(jeonhaUrl + '/signUp', {
+      method: "post",
+      headers:  {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Access-Control-Allow-Origin": "*"
+    },
+      body: bodySignUP
+  })
+  .then(res => {
+      if (res.status === 200) {
+          // 정상 작동
+          console.log('Success!');
+      } else if(res.status === 400) {
+          // 실패시
+          console.log('Failed!');
+          res.text()
+      }
+  })
+  .then(data => {
+      // 아이디 중복, 패스워드 중복, 그 외 에러
+      const overlapId = JSON.parse(data)[0];
+      const overlapPw = JSON.parse(data)[1];
+      const errorDB = JSON.parse(data)[2];
+  })
+  }
+
   render() {
     return (
       <div className = "Root">
