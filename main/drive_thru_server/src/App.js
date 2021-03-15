43d36e998092c1b6fdf2de00d4063bb3965986c7
kijiyeon <rlwldus305@naver.com>
2021-03-16 01:30:00 +0900

    socket.io 추가전

--- main/drive_thru_server/src/App.js
+++ main/drive_thru_server/src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://dtserver.azurewebsites.net"
           target="_blank"
           rel="noopener noreferrer"
         >
