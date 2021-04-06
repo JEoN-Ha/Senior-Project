89809e6ec720360cd65e698b8b6a2fff252fb201
dkfma4915 <52308823+dkfma4915@users.noreply.github.com>
2021-04-07 00:06:53 +0900

    디플로이 오류 수정중

--- main/drive_thru_web/src/store.js
+++ main/drive_thru_web/src/store.js
@@ -9,7 +9,9 @@ export default createStore(function(state, action){
             PW_state:false,
             orderName:'상품을 선택해주세요.', orderCount:0,orderID:null,
             customerType:null, isCarNumberDisabled:null,
-            jeonhaUrl: 'https://vincenzzo.azurewebsites.net'
+            // jeonhaUrl: 'https://vincenzzo.azurewebsites.net'
+            // jeonhaUrl: 'http://localhost:4000'
+            jeonhaUrl: 'https://armfulserver.azurewebsites.net'
         }
     }
 
