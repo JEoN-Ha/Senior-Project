c2e5634c148c76c2e41e6f73ebac7368bfcb313f
fora22 <48875566+fora22@users.noreply.github.com>
2021-05-05 12:29:29 +0900

    cam update

--- main/Hardware/Rasp/Cam.py
+++ main/Hardware/Rasp/Cam.py
@@ -1,1 +1,19 @@
 import cv2
+
+cap = cv2.VideoCapture(0)
+if cap.isOpened():
+    while True:
+        ret, frame = cap.read()
+        if ret:
+            cv2.imshow("cam", frame) # showing Image
+            if cv2.waitKey(1) != -1:
+                cv2.imwrite("./img/photo.png", frame)
+                break
+            else:
+                print("no frame")
+                break
+else:
+    print("can't open camera")
+
+cap.release()
+cv2.destroyAllWinodws()
