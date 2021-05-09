488c8c177fb94d5b55d9ecaad15a5a27db55a9e8
fora22 <48875566+fora22@users.noreply.github.com>
2021-05-09 21:30:15 +0900

    Create CamPostion.py

---
+++ main/Hardware/Rasp/CamPostion.py
@@ -0,0 +1,18 @@
+import cv2
+import os
+
+videoNumber = 0
+
+cap = cv2.VideoCapture(videoNumber)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,480)
+cap.set(cv2.CAP_PROP_FPS, 30)
+
+if self.cap.isOpened():
+    imgName = 'A_Postion_' + videoNumber +'.jpg'
+    ret, frame = self.cap.read()
+    # cv2.imshow("cam", frame) # showing Image
+    cuurentfolder = os.path.join(os.path.dirname(os.path.abspath('Cam.py')))
+    imgPath = cuurentfolder + "/img/" + imgName
+    cv2.imwrite(imgPath, frame)
+    
