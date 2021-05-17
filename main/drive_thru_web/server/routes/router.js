60b43f259dcafcb0cdfc71cfacdd8e92d935d50f
dkfma4915 <52308823+dkfma4915@users.noreply.github.com>
2021-05-18 00:54:20 +0900

    차량번호 분리해서 주문하기

--- main/drive_thru_web/server/routes/router.js
+++ main/drive_thru_web/server/routes/router.js
@@ -207,7 +207,6 @@ router.post('/getUserInfo', (req, res) => {
         db.query(sqlCodeToCar, (err,results) => {
             const carData = JSON.parse(JSON.stringify(results))
             if (!err) {
-                console.log(carData);
                 res.status(200).json({
                     user: userData,
                     car: carData,
@@ -304,7 +303,9 @@ router.get('/getLastOrderNo', (req, res) => {
 router.post('/order', (req, res) => {
     const orderNo = `'${req.body.orderNo}'`;
     const userwebid = `'${req.body.userWebId}'`;
-    const carid = `'${req.body.carid}'`;
+    const carid_first = `'${req.body.carid_first}'`;
+    const carid_mid = `'${req.body.carid_mid}'`;
+    const carid_end = `'${req.body.carid_end}'`;
     const payment = `${req.body.payment}`;
 
     console.log(orderNo);
@@ -314,8 +315,8 @@ router.post('/order', (req, res) => {
     let currentOrderNo = 0;
 
     const sqlCodeToOrderTable = `
-    insert into ordertable(OrderNo,OrderWebId, WebCarId, OrderPayment)
-    values (${orderNo},${userwebid}, ${carid}, ${payment});`;
+    insert into ordertable(OrderNo,OrderWebId, WebCarId_first, WebCarId_mid, WebCarId_end, OrderPayment)
+    values (${orderNo},${userwebid}, ${carid_first}, ${carid_mid}, ${carid_end}, ${payment});`;
 
     db.query(sqlCodeToOrderTable, (err, results) => {
         if (err) {
